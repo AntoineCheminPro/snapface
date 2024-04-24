681762f9e72382d3b76ed703ec13b8c42135e324
Antoine Chemin <Antoine@MacBook-Pro-Antoine.local>
2024-04-24 08:39:05 +0200

    ngFor added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FaceSnapModule } from './face-snap/face-snap.component';
-import { FaceSnap } from './models/face-snap.component'; 
+import { FaceSnap } from './models/face-snap.models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FaceSnapModule],
+  imports: [RouterOutlet, FaceSnapModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  mySnap2!: FaceSnap;
-  mySnap3!: FaceSnap;
+  mySnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = new FaceSnap('Baxter', 'Le chien qui pense ... pas', new Date(), 0, 'https://media.istockphoto.com/id/450726311/fr/photo/retriever-du-labrador-12-mois-assis.jpg?s=2048x2048&w=is&k=20&c=GAYEzTnKjdN6G8Vk_1K3_9wWP3vytfwTYwPNaugYw4g=');
-    this.mySnap2 = new FaceSnap('Ulysse', 'Le chien qui voyageais', new Date(), 20, 'https://media.istockphoto.com/id/1257649913/fr/photo/chiot-noir-de-retriever-de-labrador-restant.jpg?s=2048x2048&w=is&k=20&c=9IJq5OFm-cpcVV9AIcleklxEItvKHNxgsBq4zxw4zu8=');
-    this.mySnap3 = new FaceSnap('Olympe', 'Le petit chienchien', new Date(), 10, 'https://media.istockphoto.com/id/500871708/fr/photo/chien-de-race-de-bouledogue-fran%C3%A7ais.jpg?s=2048x2048&w=is&k=20&c=c1PDZmDxiJ2WsCl1GU15PiP6X9kuR-BZZ1xCXDyMras=');
+    this.mySnaps = [
+      {
+        title: 'Baxter',
+        description: 'Le chien qui pense ... pas',
+        createdDate: new Date(),
+        snaps: 5,
+        imageUrl:
+          'https://media.istockphoto.com/id/450726311/fr/photo/retriever-du-labrador-12-mois-assis.jpg?s=2048x2048&w=is&k=20&c=GAYEzTnKjdN6G8Vk_1K3_9wWP3vytfwTYwPNaugYw4g=',
+        location: '6 feet under',
+      },
+
+      {
+        title: 'Ulysse',
+        description: 'Le chien qui voyageais',
+        createdDate: new Date(),
+        snaps: 20,
+        imageUrl:
+          'https://media.istockphoto.com/id/1257649913/fr/photo/chiot-noir-de-retriever-de-labrador-restant.jpg?s=2048x2048&w=is&k=20&c=9IJq5OFm-cpcVV9AIcleklxEItvKHNxgsBq4zxw4zu8=',
+        location: '6 feet under',
+      },
+
+      {
+        title: 'Olympe',
+        description: 'Le petit chienchien',
+        createdDate: new Date(),
+        snaps: 10,
+        imageUrl:
+          'https://media.istockphoto.com/id/500871708/fr/photo/chien-de-race-de-bouledogue-fran%C3%A7ais.jpg?s=2048x2048&w=is&k=20&c=c1PDZmDxiJ2WsCl1GU15PiP6X9kuR-BZZ1xCXDyMras=',
+        location: 'Sur le canapé',
+      },
+    ];
   }
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,50 +1,45 @@
 import { Component, Input, NgModule, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { FaceSnap } from '../models/face-snap.models';
 
 @Component({
-    selector: 'app-face-snap',
-    templateUrl: './face-snap.component.html',
-    styleUrls: ['./face-snap.component.scss']
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss'],
 })
 export class FaceSnapComponent implements OnInit {
-    @Input() faceSnap!: FaceSnap;
+  @Input() faceSnap!: FaceSnap;
 
-    buttonText: string = 'Oh snap!';
+  buttonText: string = 'Oh snap!';
 
-    ngOnInit(): void {
+  ngOnInit(): void {}
+
+  constructor(private datePipe: DatePipe) {}
+
+  onClic() {
+    if (this.buttonText === 'Oh snap!') {
+      this.faceSnap.snaps++;
+      this.buttonText = 'Oups unsnap!';
+    } else {
+      this.faceSnap.snaps--;
+      this.buttonText = 'Oh snap!';
     }
-
-    constructor(private datePipe: DatePipe) { }
-
-    onClic() {
-        if (this.buttonText === 'Oh snap!') {
-            this.faceSnap.snaps++;
-            this.buttonText = 'Oups unsnap!';
-        } else {
-            this.faceSnap.snaps--;
-            this.buttonText = 'Oh snap!';
-        }
-    };
-
-
+  }
 }
-
-import { CommonModule } from '@angular/common';
-import { FaceSnap } from '../models/face-snap.component';
 
 @NgModule({
-    declarations: [FaceSnapComponent],
-    imports: [CommonModule],
-    providers: [DatePipe],
-    exports: [FaceSnapComponent]
+  declarations: [FaceSnapComponent],
+  imports: [CommonModule],
+  providers: [DatePipe],
+  exports: [FaceSnapComponent],
 })
-export class FaceSnapModule { }
+export class FaceSnapModule {}
 
 function ngOnInit() {
-    throw new Error('Function not implemented.');
+  throw new Error('Function not implemented.');
 }
 
-
 function onSnap() {
-    throw new Error('Function not implemented.');
+  throw new Error('Function not implemented.');
 }
